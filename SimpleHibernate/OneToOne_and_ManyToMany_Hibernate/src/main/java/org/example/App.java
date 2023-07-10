50df66c170676e69f4abbcdcf29d6d65d6789349
lapochka2019 <55483949+lapochka2019@users.noreply.github.com>
2023-07-10 22:03:19 +0300

    Add files via upload
    Добавлен проект Maven, Hibernate, Java 9 для отработки отношений "Один к одному" и "Многие ко многим". 
    Многие ко многим не работают, проверить ошибку
    Один к одному работают корректно

---
+++ SimpleHibernate/OneToOne_and_ManyToMany_Hibernate/src/main/java/org/example/App.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.example.model.manyToMany.Actor;
+import org.example.model.manyToMany.Movie;
+import org.example.model.oneToOne.Passport;
+import org.example.model.oneToOne.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        //Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        //try with resources
+        //Java 9
+        try(sessionFactory){
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+
+            /**Person person = new Person("First Person", 14);
+            Passport passport = new Passport(person, 123456);
+            person.setPassport(passport);
+            session.save(person);**/
+
+            Movie movie = new Movie("Pulp fiction", 1998);
+            Actor actor1 = new Actor("Harvey Keitel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson",72);
+
+            movie.setActors(new ArrayList<>(List.of(actor1,actor2)));
+            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+
+            session.save(movie);
+            session.save(actor1);
+            session.save(actor2);
+
+            session.getTransaction().commit();
+
+        }
+    }
+}
