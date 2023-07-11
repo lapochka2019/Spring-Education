11f9b70844f6fac243bdc514759a1c2704f19a80
lapochka2019 <55483949+lapochka2019@users.noreply.github.com>
2023-07-11 21:46:22 +0300

    Add files via upload
    Загружен проект с отношением таблиц Один к одному и Многие ко многим

---
+++ SimpleHibernate/OneToOne_and_ManyToMany_Hibernate/src/main/java/org/example/model/manyToMany/Movie.java
@@ -0,0 +1,69 @@
+package org.example.model.manyToMany;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Movie")
+public class Movie {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "year_of_production")
+    private int year_of_production;
+
+    @ManyToMany(mappedBy = "movies")
+    private List<Actor> actors;
+
+    public Movie() {
+    }
+
+    public Movie(String title, int year_of_production) {
+        this.title = title;
+        this.year_of_production = year_of_production;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int movie_id) {
+        this.id = movie_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear_of_production() {
+        return year_of_production;
+    }
+
+    public void setYear_of_production(int year_of_production) {
+        this.year_of_production = year_of_production;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movie_id=" + id +
+                ", title='" + title + '\'' +
+                ", year_of_production=" + year_of_production +
+                '}';
+    }
+}
